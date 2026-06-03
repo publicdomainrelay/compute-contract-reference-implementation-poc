f2a534ad34ab83b88a326078191ec86ff73399f6
John Andersen <johnandersenpdx@gmail.com>
2026-06-03 04:28:36 -0700

    refactor(typescript): bump
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/typescript/qemu/run.sh
+++ src/typescript/qemu/run.sh
@@ -1,1 +1,3 @@
 THIS_ENDPOINT=https://homelab--johnandersen777-bsky-social.fedproxy.com TEAM_UUID=5svqtrhheairglgiiyvutzik PORT=9000 deno run -A --watch ./main.ts 2>&1 | jq --unbuffered -rR '(fromjson? // .)'
+
+
