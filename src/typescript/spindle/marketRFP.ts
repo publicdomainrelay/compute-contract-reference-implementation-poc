a952316d8352b279ecddc521d1bdaa7c02c4f01e
John Andersen <johnandersenpdx@gmail.com>
2026-06-04 15:02:27 -0700

    refactor(typescript): correct rbac aud for ATProto reverse proxy
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/typescript/spindle/marketRFP.ts
+++ src/typescript/spindle/marketRFP.ts
@@ -556,7 +556,7 @@ export async function marketRFPSubmitWorkflow(
         role_name: serviceName,
         definition: {
           // aud is us
-          aud: `api://ATProto?actx=${winner.config.actx}`,
+          aud: `api://ATProto?actx=${agentDid}`,
           iss: `${winner.config.issuer_uri}`,
           // actx is them for this simple example
           // TODO Thi should be templating winner.config.subject
