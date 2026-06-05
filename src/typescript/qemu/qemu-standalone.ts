d312b49653145fa0645053e64a348c1cbea1bf37
John Andersen <johnandersenpdx@gmail.com>
2026-06-04 23:49:34 -0700

    feat(fedproxy-client): ubuntu needed sudo
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/typescript/qemu/qemu-standalone.ts
+++ src/typescript/qemu/qemu-standalone.ts
@@ -120,6 +120,7 @@ async function installFedora(chrootDir: string): Promise<void> {
     "openssh-server",
     "vim",
     "tmux",
+    "sudo",
     "jq",
   ]);
 }
@@ -131,7 +132,7 @@ async function installUbuntu(chrootDir: string): Promise<void> {
   await nspawn(
     "ln -sf /usr/share/zoneinfo/UTC /etc/localtime && " +
     "DEBIAN_FRONTEND=noninteractive apt-get update && " +
-    "DEBIAN_FRONTEND=noninteractive apt-get install -y systemd linux-image-generic cloud-init dracut btrfs-progs util-linux rsyslog openssh-server vim tmux ca-certificates curl jq",
+    "DEBIAN_FRONTEND=noninteractive apt-get install -y systemd linux-image-generic cloud-init dracut btrfs-progs util-linux rsyslog openssh-server vim tmux ca-certificates curl jq sudo locales",
   );
   await nspawn(
     "install -m 0755 -d /etc/apt/keyrings && " +
